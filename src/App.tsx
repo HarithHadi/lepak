55a17700018d45f1169742bcef0b6099c2a90a58
aridontknowcode <m.harith.hadi@gmail.com>
2026-02-18 14:22:51 +0800

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Start from './pages/Start'
 import Home from './pages/Home'
 import WordGame from './pages/WordGame'
