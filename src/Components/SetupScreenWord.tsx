801aa0905c87c06dc2636133d3982ca5628e623c
aridontknowcode <m.harith.hadi@gmail.com>
2026-03-06 17:01:13 +0800

    added more impostors

--- src/Components/SetupScreenWord.tsx
+++ src/Components/SetupScreenWord.tsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 interface SetupProps {
-  onStart: (numPlayers: number) => void;
+  onStart: (numPlayers: number, ImpostorCount: number) => void;
 }
 
 export default function SetupScreenWord({ onStart }: SetupProps) {
   
   const [playerCount, setPlayerCount] = useState<number>(3);
+  const [ImpostorCount,setImpostorCount] = useState<number>(1);
+  const allowTwo = playerCount >= 5;
+  const allowThree = playerCount >= 8;
+
+  useEffect(() => {
+    if (playerCount < 5) {
+      setImpostorCount(1);
+    } 
+    else if (playerCount < 8) {
+      setImpostorCount(1); 
+    } 
+    else {
+      setImpostorCount(2); 
+    }
+  }, [playerCount]);
 
   return (
     <>
@@ -18,20 +34,57 @@ export default function SetupScreenWord({ onStart }: SetupProps) {
             How many players?
           </label>
           <input
+            placeholder="0"
             type="number"
             min="3"
             max="20"
             value={playerCount}
-            onChange={(e) => setPlayerCount(parseInt(e.target.value) || 0)}
+            onChange={(e) => setPlayerCount(parseInt(e.target.value))}
             className="w-full p-4 text-gray-700 text-2xl text-center font-bold border-2 border-gray-700 focus:border-blue-500 focus:outline-none transition"
           />
           <p className="text-gray-500 text-xs mt-2 text-center">
             Minimum 3 players required
           </p>
         </div>
+        
+        {/* Impostor count */}
+        <div className="w-full">
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            How many Impostor?
+          </label>
+          <div className="space-x-3">
+            <button
+                onClick={() => setImpostorCount(1)}
+                className={`px-4 py-2 border-2 border-black font-bold ${
+                  ImpostorCount === 1 ? "bg-[#00ED10]!" : ""
+                }`}
+              >
+                1
+            </button>
+            <button
+              disabled={!allowTwo}
+              onClick={() => setImpostorCount(2)}
+              className={`px-4 py-2 border-2 border-black font-bold ${
+                ImpostorCount === 2 ? "bg-[#00ED10]!" : ""
+              } ${!allowTwo ? "opacity-40 cursor-not-allowed" : ""}`}
+            >
+              2
+            </button>
+
+            <button
+              disabled={!allowThree}
+              onClick={() => setImpostorCount(3)}
+              className={`px-4 py-2 border-2 border-black font-bold ${
+                ImpostorCount === 3 ? "bg-[#00ED10]!" : ""
+              } ${!allowThree ? "opacity-40 cursor-not-allowed" : ""}`}
+            >
+              3
+            </button>
+          </div>
+        </div>
 
         <button
-          onClick={() => onStart(playerCount)}
+          onClick={() => onStart(playerCount, ImpostorCount)}
           className="bg-green-500 text-white px-10 py-4 w-full rounded-xl font-bold shadow-[4px_4px_0_0_rgba(0,0,0,1)] hover:shadow-none hover:translate-x-1 hover:translate-y-1 transition-all duration-50"
         >
           Start Game
