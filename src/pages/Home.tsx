8f53977bc7abf16ccc2edb24f375779d69238f99
aridontknowcode <m.harith.hadi@gmail.com>
2026-03-11 04:24:48 +0800

    addedd button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,21 +7,30 @@ export default function Home() {
     <div className="flex flex-col items-center gap-4 p-10">
       <h1 className="font-bold text-2xl text-white">Choose your game</h1>
 
-      {/* --- LOCKED GAME (Mafia) --- */}
-      <div className="relative group">
-        {/* The Chain/Tape Overlay */}
+      {/* <div className="relative group">
+
         <div className="absolute inset-0 z-10 flex items-center justify-center pointer-events-none">
           <div className="bg-yellow-400 text-black text-[10px] font-black px-4 py-0.5 uppercase tracking-tighter border-y border-black -rotate-12 shadow-md">
             Locked
           </div>
         </div>
 
-        {/* The Button */}
+
         <button 
-          disabled
           className="bg-gray-300 text-gray-500 border-2 border-dashed border-gray-400 px-10 py-4 rounded-xl grayscale opacity-60 cursor-not-allowed flex items-center gap-2"
+          onClick={() => navigate("/Mafia")}
         >
           Mafia 🔒
+        </button>
+      </div> */}
+
+
+      <div className="p-4">
+        <button 
+          className="bg-green-500 text-white px-10 py-4 rounded-xl font-bold shadow-[4px_4px_0_0_rgba(0,0,0,1)] hover:shadow-none hover:translate-x-1 hover:translate-y-1 transition-all" 
+          onClick={() => navigate("/Mafia")}
+        >
+          Mafia
         </button>
       </div>
 
